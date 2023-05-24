290723744006a2aed481583abfce3289b7a06a31
Dhiraj077 <93478391+Dhiraj077@users.noreply.github.com>
2023-05-25 00:34:23 +0530

    about build started

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,22 +1,50 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+// import '../components/test.css'
 
 function about() {
   return (
     <div>
       <Navbar></Navbar>
       <section className="text-gray-600 body-font">
-        <div class="container mx-auto flex px-5 py-12 items-center justify-center flex-col">
-          <img className='lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded' src="https://images.unsplash.com/photo-1556157382-97eda2d62296?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
-            <div className="text-center lg:w-2/3 w-full">
-              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Growth your business with us</h1>
-              <p className="mb-8 leading-relaxed">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda maiores ipsa aliquid, perspiciatis, asperiores quis similique eum, provident ab quos officia dicta exercitationem at expedita eligendi consequatur earum. Pariatur, molestiae!</p>
-              <div className="flex justify-center">
-                <button className="inline-flex text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded text-lg">Know More</button>
+        <div class="container my-18 px-6 mx-auto">
+
+          <section class="mb-28 ">
+            <div class="px-6 py-12 md:px-12 text-center lg:text-left">
+              <div class="container mx-auto xl:px-32">
+                <div class="grid lg:grid-cols-2 gap-12 flex items-center md:max-w-[1020px]">
+                  <div class="mt-12 lg:mt-0">
+                    <h1 class="text-5xl md:text-5xl xl:text-4xl font-bold tracking-tight mb-12 text-white" >What We're All About</h1>
+                    <p className='tracking-tight mb-2 text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ullam optio enim quas asperiores aut obcaecati sit delectus voluptates sint? Obcaecati optio labore tempore assumenda laboriosam corrupti cum esse perspiciatis laudantium ipsa doloremque id magni,!</p>
+                  </div>
+                  <div class="mb-12 lg:mb-0">
+                    <img className=" w-full rounded-lg shadow-lg" src={require('../images/img_9.jpg')} alt="img" />
+                  </div>
+                </div>
               </div>
             </div>
+          </section>
+          <section class="mb-32 text-gray-800">
+            <div className='justify-center text-center mb-4 text-4xl text-white '>Made in India</div>
+            <img className=" w-full rounded-lg shadow-lg mb-6" src={require('../images/img_18.jpg')} alt="img" />
+
+            <h1 class="font-bold text-3xl mb-6">Hand Build</h1>
+
+            <p className='text-white'>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi harum tempore
+              cupiditate asperiores provident, itaque, quo ex iusto rerum voluptatum delectus corporis
+              quisquam maxime a ipsam nisi sapiente qui optio! Dignissimos harum quod culpa officiis
+              suscipit soluta labore! Expedita quas, nesciunt similique autem, sunt, doloribus pariatur
+              maxime qui sint id enim. Placeat, maxime labore. Dolores ex provident ipsa impedit, omnis
+              magni earum. Sed fuga ex ducimus consequatur corporis, architecto nesciunt vitae ipsum
+              consequuntur perspiciatis nulla esse voluptatem quos dolorum delectus similique eum vero
+              in est velit quasi pariatur blanditiis incidunt quam.
+            </p>
+          </section>
         </div>
       </section>
+      <Footer></Footer>
     </div>
   )
 }
--- src/pages/company.jsx
+++ src/pages/company.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 const company = () => {
   return (
     <div>
         <Navbar></Navbar>
         <div>Company</div>
+        <Footer></Footer>
     </div>
   )
 }
